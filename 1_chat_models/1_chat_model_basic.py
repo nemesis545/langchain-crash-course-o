b31991f7e3e7b13ffc5a200efc53e405c17c6c3a
alex rincon <nemessis545@gmail.com>
2024-09-25 19:57:14 -0500

    initial commit to adapt to ollama from chatgpt

--- 1_chat_models/1_chat_model_basic.py
+++ 1_chat_models/1_chat_model_basic.py
@@ -1,18 +1,26 @@
 # Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/
 # OpenAI Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/openai/
 
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-
+# from dotenv import load_dotenv
+# from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
 # Load environment variables from .env
-load_dotenv()
+# load_dotenv()
 
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+# model = ChatOpenAI(model="gpt-4o")
+model = OllamaLLM(model="llama3.1")
+template = """Question: {question}
 
+Answer: Let's think step by step."""
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+result = chain.invoke({"question": "What is 81 divided by 9?"})
 # Invoke the model with a message
-result = model.invoke("What is 81 divided by 9?")
+# result = model.invoke("")
 print("Full result:")
 print(result)
-print("Content only:")
-print(result.content)
+# print("Content only:")
+# print(dir(result))
